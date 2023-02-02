52c6fabde030e385b652b3f0cd8bfa15181cd120
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-02 01:57:39 +0100

    pour redemarrer avec la methode addEventListener

--- morpion.js
+++ morpion.js
@@ -1,3 +1,9 @@
 addEventListener("DOMContentLoaded",  () => {
-
+    const tiles = Array.from(document.querySelectorAll('.tile'));
+    const joueurDisplay =document.querySelector('.display-joueur');
+    const redemarrerButton = document.querySelector('.redemarrer')
+    redemarrerButton.addEventListener('click', redemarrerBoard);
 });
+
+//addEventListener gère des évènements.
+//Ici, l'événement DOMContentLoaded se déclenche lorsque tous les scripts ont été executés (il n'attend pas que le reste finisse de se charger).
