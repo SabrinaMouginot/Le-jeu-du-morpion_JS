bfc750ad6ab32d254eb3fb52e67af8ab3a91b6c1
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-02 03:10:32 +0100

    creation d un tableau avec l ensemble des possibilites gagnantes

--- morpion.js
+++ morpion.js
@@ -18,6 +18,16 @@ addEventListener("DOMContentLoaded",  () => {
          [6] [7] [8]
     */
 
+    const conditionsGagnantes = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
          
     redemarrerButton.addEventListener('click', redemarrerBoard);
 });
