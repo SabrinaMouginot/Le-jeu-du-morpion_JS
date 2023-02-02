6ce5db582f631313ea8ba6cb11a2858fc9aac547
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-02 02:13:37 +0100

    commentaire sur l'indexation dans le tableau

--- morpion.js
+++ morpion.js
@@ -7,6 +7,18 @@ addEventListener("DOMContentLoaded",  () => {
     let joueurActuel = 'X';
     let isGameActive = true;
 
+    const JOUEURX_GAGNE = 'JOUEURX_GAGNE';
+    const JOUEURO_GAGNE = 'JOUEURO_GAGNE';
+    const TIE = 'TIE';
+
+    /*
+         Indexation dans le tableau
+         [0] [1] [2]
+         [3] [4] [5]
+         [6] [7] [8]
+    */
+
+         
     redemarrerButton.addEventListener('click', redemarrerBoard);
 });
 
