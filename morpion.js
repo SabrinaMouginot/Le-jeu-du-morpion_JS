1d6b3d8e75e4bb9c5398c5da7eb5999b9750b689
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-02 03:23:45 +0100

    je mets en place l action du joueur

--- morpion.js
+++ morpion.js
@@ -1,4 +1,6 @@
 addEventListener("DOMContentLoaded",  () => {
+    //addEventListener gère des évènements.
+    //Ici, l'événement DOMContentLoaded se déclenche lorsque tous les scripts ont été executés (il n'attend pas que le reste finisse de se charger).
     const tiles = Array.from(document.querySelectorAll('.tile'));
     const joueurAffiche =document.querySelector('.display-joueur');
     const redemarrerButton = document.querySelector('.redemarrer');
@@ -28,9 +30,22 @@ addEventListener("DOMContentLoaded",  () => {
         [0, 4, 8],
         [2, 4, 6],
     ];
+
+    const ActionUtilisateur = (tile, index) => {
+        if(isValidAction(tile)&& isGameActive) {
+            tile.innerText = joueurActuel,
+            tile.classList.add(`joueur${joueuractuel}`);
+            updateBoard(index);
+            handleResultValidation();
+            changePlayer();
+        }
+    }
+
+    tiles.forEach( (tile, index) => {
+        tile.addEventListener('click', () => ActionUtilisateur ( tile, index ));
+    
+    });
          
     redemarrerButton.addEventListener('click', redemarrerBoard);
 });
 
-//addEventListener gère des évènements.
-//Ici, l'événement DOMContentLoaded se déclenche lorsque tous les scripts ont été executés (il n'attend pas que le reste finisse de se charger).
